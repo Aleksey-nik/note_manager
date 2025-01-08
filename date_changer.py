c4b30f80cf39db54af7b50c1635252d100fcf5df
Aleksey-nik <k-susorov@mail.ru>
2025-01-08 17:26:55 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,16 @@
+username="Алексей"
+title="Программа 1"
+content="Вычисление"
+status="Начало"
+created_date_day="день"
+created_date_month="- месяц"
+created_date_year="- год"
+issue_date_day="день"
+issue_date_month="- месяц"
+issue_date_year="- год"
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date_day, created_date_month)
+print("Дата истечения заметки:", issue_date_day, issue_date_month)
