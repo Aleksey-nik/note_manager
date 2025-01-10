3c9bfd7959b15b1cb2c247c12e3d589032dce287
Aleksey-nik <k-susorov@mail.ru>
2025-01-10 20:22:20 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,23 @@
+#username="Алексей"
+#title="Программа 1"
+#content="Вычисление"
+#status="Начало"
+#created_date_day="день"
+#created_date_month="- месяц"
+#created_date_year="- год"
+#issue_date_day="день"
+#issue_date_month="- месяц"
+#issue_date_year="- год"
+username=input("Введите имя пользователя:")
+print("Имя пользователя:", username)
+title=input("Введите заговок заметки:")
+print("Заголовок заметки:", title)
+content=input("Введите описание заметки:")
+print("Описание заметки:", content)
+status=input("Введите статус заметки:")
+print("Статус заметки:", status)
+created=input("Введите дату создания,день,месяц,год:")
+print("Дата создания заметки:", created)
+issue=input("Введите дату истечеия,день,месяц,год:")
+print("Дата истечения заметки:", issue)
+
