1f2abac4be088c202e73ef3e33ea8d343f278412
Aleksey-nik <k-susorov@mail.ru>
2025-01-28 10:49:57 +0300

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -2,20 +2,17 @@
 #title="Программа 1"
 #content="Вычисление"
 #status="Начало"
-#created_date_day="день"
-#created_date_month="- месяц"
-#created_date_year="- год"
-#issue_date_day="день"
-#issue_date_month="- месяц"
-#issue_date_year="- год"
+#created_date="день.месяц.год"
+#issue_date="день.месяц.год"
+title=["Заголовок заметки 1", "Заголовок заметки 2", "Заголовок заметки 3" ]
 username=input("Введите имя пользователя:")
 print("Имя пользователя:", username)
 title1=input("Введите заговок заметки 1:")
-print("Заголовок заметки 1:", title1)
+print(title[0], title1)
 title2=input("Введите заговок заметки 2:")
-print("Заголовок заметки 2:", title2)
+print(title[1], title2)
 title3=input("Введите заговок заметки 3:")
-print("Заголовок заметки 3:", title3)
+print(title[2], title3)
 content=input("Введите описание заметки:")
 print("Описание заметки:", content)
 status=input("Введите статус заметки:")
