ca4dfab64eaba74783e9d9f9e5f23e6871316f1b
Aleksey-nik <k-susorov@mail.ru>
2025-01-29 10:01:31 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -2,11 +2,11 @@ username="Алексей"
 title="Программа 1"
 content="Вычисление"
 status="Начало"
-created_date="день.месяц.год"
-issue_date="день.месяц.год"
+created_date="25.01.2025"
+issue_date="30.01.2025"
 print("Имя пользователя:", username)
 print("Заголовок заметки:", title)
 print("Описание заметки:", content)
 print("Статус заметки:", status)
-print("Дата создания заметки:", created_date [:11])
-print("Дата истечения заметки:", issue_date [:11])
+print("Дата создания заметки:", created_date [:6])
+print("Дата истечения заметки:", issue_date [:6])
